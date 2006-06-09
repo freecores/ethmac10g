a2fdf1604c1ed4316b12e8facd6690870815bb82
fisher5090 <fisher5090@ce6fc7eb-4d63-44a3-84cb-a7ee523b59cd>
2006-06-09 04:12:56 +0000

    good version

--- rtl/verilog/tx_engine/byte_counter.v
+++ rtl/verilog/tx_engine/byte_counter.v
@@ -15,17 +15,18 @@ reg [15:0] counter;
 always @(posedge CLK or posedge RESET)
 begin
    if (RESET == 1) begin
-	counter = 16'h0000;
+	   counter = 16'h0000;
    end
-
    // the ack is delayed which starts the counter
    else if (START == 1) begin
        counter = counter + 8;
    end
-
-   BYTE_COUNTER = counter;
 end
 
+always @(posedge CLK)
+begin
+   BYTE_COUNTER = counter;
+end
 
 endmodule // End of Module 
 
